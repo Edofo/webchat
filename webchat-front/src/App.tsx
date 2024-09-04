7e77576faeb4814ebb62cc83550e34f73c636c9b
Hivenet-Nolan <144666332+Hivenet-Nolan@users.noreply.github.com>
2024-09-04 14:24:19 +0200

    fixes

--- webchat-front/src/App.tsx
+++ webchat-front/src/App.tsx
@@ -1,13 +1,15 @@
 import './App.css'
 
-import { Suspense } from 'react'
-import { Routes } from 'react-router-dom'
+import { lazy, Suspense } from 'react'
+
+const AuthHomePage = lazy(() => import('@pages/Home'))
 
 const App = () => {
   return (
     <div>
       <Suspense fallback={'Loading..'}>
-        <Routes>{/* <Route path="/" element={<Home />} /> */}</Routes>
+        {/* <Routes><Route path="/" element={<Home />} /> </Routes> */}
+        <AuthHomePage />
       </Suspense>
     </div>
   )
---
+++ webchat-front/src/pages/Home.tsx
@@ -0,0 +1,17 @@
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  )
+}
+
+const AuthHomePage = () => {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  )
+}
+
+export default AuthHomePage
